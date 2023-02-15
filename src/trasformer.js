21d8ba378a8f56ac243106421747e459748a17c5
coderaiser <coderaiser@cloudcmd.io>
2023-02-15 20:23:48 +0200

    feature: mobile-putout-editor: add version

--- src/trasformer.js
+++ src/trasformer.js
@@ -34,7 +34,7 @@ export const createTransform = async ({type, value, setInfo, setFinalTransform, 
         exports,
     };
     
-    const pluginPutout = (await import('https://esm.sh/@putout/bundle/plugin-putout')).default;
+    const pluginPutout = (await import('https://esm.sh/@putout/bundle@1.4/plugin-putout')).default;
     const pluginDeclare = (await import('https://esm.sh/@putout/plugin-declare-undefined-variables?alias=putout:@putout/bundle')).default;
     const pluginDeclareBeforeReference = (await import('https://esm.sh/@putout/plugin-declare-before-reference?alias=putout:@putout/bundle')).default;
     const pluginConvertConstToLet = (await import('https://esm.sh/@putout/plugin-convert-const-to-let?alias=putout:@putout/bundle')).default;
